e587f83c323c6e83c2bf75fc40714404d50cfebe
Sadra Shameli <sadra.shameli1@gmail.com>
2024-05-23 13:54:33 +0200

    bug fix

--- app/settings.py
+++ app/settings.py
@@ -2,10 +2,10 @@ import os
 import environ
 from pathlib import Path
 
-env = environ.Env()
-environ.Env.read_env()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(env_file=str(BASE_DIR) + "/.env")
 
 DEBUG = env("DEBUG", default=False)
 
