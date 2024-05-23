486b222c12a542e0fde3f3eee9b1d19a9828b0a9
Sadra Shameli <sadra.shameli1@gmail.com>
2024-05-23 12:53:55 +0200

    test

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,4 @@
+import os
 import environ
 from pathlib import Path
 
@@ -52,7 +53,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "app.wsgi.app"
+WSGI_APPLICATION = "app.wsgi.application"
 
 SECRET_KEY = env("SECRET_KEY")
 
@@ -94,9 +95,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_URL = "static/"
 
-STATICFILES_DIR = BASE_DIR / "static"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
--- app/wsgi.py
+++ app/wsgi.py
@@ -5,4 +5,5 @@ from . import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
-app = WhiteNoise(get_wsgi_application(), root=settings.STATIC_ROOT)
+application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
