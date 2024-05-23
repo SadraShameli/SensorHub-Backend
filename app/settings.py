14506fb4fcadd136ed9a96eea8bbc3e8e75af733
Sadra Shameli <sadra.shameli1@gmail.com>
2024-05-23 12:41:30 +0200

    test

--- app/settings.py
+++ app/settings.py
@@ -59,11 +59,11 @@ SECRET_KEY = env("SECRET_KEY")
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
+        "NAME": env("PGDATABASE"),
+        "USER": env("PGUSER"),
+        "PASSWORD": env("PGPASSWORD"),
+        "HOST": env("PGHOST"),
+        "PORT": env("PGPORT"),
     }
 }
 
