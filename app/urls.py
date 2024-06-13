558ebb28faf53ab9fecc823a0edf383be4c2fc4b
Sadra <sadra.shameli1@gmail.com>
2024-06-13 07:34:22 +0200

    Removed unused import

--- app/urls.py
+++ app/urls.py
@@ -4,10 +4,10 @@ from django.urls import path, include
 from app import settings
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
-    path("", include("api.urls")),
+    path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    path("", include("api.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
